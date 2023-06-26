aa1c314778f121d3c686f79469226465251696aa
2chloee <2chloeee@gmail.com>
2023-06-26 10:46:50 -0700

    Initial Commit

---
+++ ASL_translator/ASL_translatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASL_translatorApp.swift
+//  ASL_translator
+//
+//  Created by Chloe Low on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ASL_translatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
