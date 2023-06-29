3e0d426e0c6f67cd2b1390051da4e742534c1561
2chloee <97809774+2chloee@users.noreply.github.com>
2023-06-29 11:48:56 -0700

    enter date and get word of the day

--- ASL_translator/WordOfTheDay.swift
+++ ASL_translator/WordOfTheDay.swift
@@ -1,20 +1,98 @@
 //
-//  WordOfTheDay.swift
+//  ContentView.swift
 //  ASL_translator
 //
-//  Created by Chloe Low on 6/27/23.
+//  Created by Chloe Low on 6/26/23.
 //
 
 import SwiftUI
 
-struct WordOfTheDay: View {
+struct ContentView: View {
+    @State private var text: String = ""
+    let signs =
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+        "y", "z"]; // for translating text into sign language
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+        
+        ZStack {
+            
+            VStack {
+                
+                Text("ASL Translator")
+                    .font(.largeTitle)
+                    .fontWeight(.light)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                
+                    .toolbar{
+                        NavigationLink(destination: WordOfTheDay()) {
+                            Text("Word of The Day")
+                                .font(.footnote)
+                                .fontWeight(.light)
+                                .multilineTextAlignment(.leading)
+                        }
+                        
+                        NavigationLink(destination: HistoryOfASL()) {
+                            Text("History")
+                                .font(.footnote)
+                                .fontWeight(.light)
+                        }
+                                                
+                        NavigationLink(destination: Famous_DHOH_People()) {
+                            Text("Famous DHOH")
+                                .font(.footnote)
+                                .fontWeight(.light)
+                        }
+                        
+                        NavigationLink(destination: Alphabet()) {
+                            Text("Alphabet")
+                                .font(.footnote)
+                                .fontWeight(.light)
+                        }
+                    }
+                
+            }
+            VStack(){
+                
+                TextField(
+                    "Enter text to be translated...",
+                    text: $text, axis: .vertical
+                )                       .font(.body)
+                    .fontWeight(.regular)
+                    .multilineTextAlignment(.leading)
+                    .fontWeight(.regular)
+                    
+                
+                    .frame(maxHeight: .infinity, alignment: .center)
+                    .padding()
+                    .background(Rectangle()
+                        .foregroundColor(.white))
+                    .cornerRadius(15)
+                    .shadow(radius: 20)
+                    .padding()
+                    .padding()
+                    .padding()
+                    .padding()
+                
+                
+                Button("Submit") {
+                    
+                }
+                
+            }
+            
+            }
+            
+        }
+        
     }
 }
 
-struct WordOfTheDay_Previews: PreviewProvider {
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        WordOfTheDay()
+        ContentView()
     }
 }
